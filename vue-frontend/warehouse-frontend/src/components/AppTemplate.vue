8e027fefa680c3b84939d6c40180ba7dc0e9aa10
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2025-01-27 14:09:30 +0000

    refactoring

---
+++ vue-frontend/warehouse-frontend/src/components/AppTemplate.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="container mt-3">
+    <div class="mb-3">
+      <label class="fw-bold">Operator Name:</label>
+      <input type="text" class="form-control d-inline w-auto" :disabled="isOperatorDisabled" v-model="operatorName" @keyup.enter="checkOperator" ref="operatorInput">
+      <label class="fw-bold ms-3">Current State:</label>
+      <input type="text" class="form-control d-inline w-auto" value="Asset Scan" disabled>
+      <button class="btn btn-secondary ms-3" style="margin-top: -5px;" disabled>Completed</button>
+      <label class="fw-bold ms-3">Mode:</label>
+      <select class="form-select d-inline w-auto" :disabled="isModeDisabled" ref="modeInput" v-model="selectedMode" @change="modeChanged">
+        <option selected>Select a mode</option>
+        <option v-for="mode in modes" :key="mode">{{ mode }}</option>
+      </select>
+    </div>
+
+    <div class="mb-3" v-if="showParent">
+      <div class="d-flex align-items-center">
+        <div class="col-auto">
+          <label class="fw-bold" for="assetId">Parent ID:</label>
+        </div>
+        <div class="col">
+          <div class="d-flex align-items-center">
+            <input type="text" class="form-control d-inline w-auto" :disabled="isParentDisabled" v-model="parentId" @keyup.enter="scanParent" ref="parentInput">
+            <span class="badge bg-success" v-if="isParentIdValid">OK</span>
+            <div id="message" class="alert alert-danger ms-2 mb-0 custom-alert" style="display: none; max-width: 300px;">
+              <!-- Error message will appear here -->
+            </div>
+            <div id="message-success" class="alert alert-success ms-2 mb-0 custom-alert" style="display: none; max-width: 300px;">
+              <!-- Success message will appear here -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-3" v-if="showAsset">
+      <div class="d-flex align-items-center">
+        <div class="col-auto">
+          <label class="fw-bold" for="assetId">Asset ID:</label>
+        </div>
+        <div class="col">
+          <div class="d-flex align-items-center">
+            <input type="text" class="form-control d-inline w-auto" v-model="assetId" @keyup.enter="scanAsset" ref="assetInput">
+            <div id="message" class="alert alert-danger ms-2 mb-0 custom-alert" style="display: none; max-width: 300px;">
+              <!-- Error message will appear here -->
+            </div>
+            <div id="message-success" class="alert alert-success ms-2 mb-0 custom-alert" style="display: none; max-width: 300px;">
+              <!-- Success message will appear here -->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Transaction Section -->
+    <h4 v-if="showTransActionsAndInventory">Transaction</h4>
+    <div class="mb-2 text-end" v-if="showTransActionsAndInventory">Asset Scan Failure Count: <input type="text" value="0" class="form-control d-inline w-auto"></div>
+
+    <table class="table table-bordered" v-if="showTransActionsAndInventory">
+      <thead>
+      <tr>
+        <th>Transaction ID</th>
+        <th>Asset ID</th>
+        <th>Equipment</th>
+        <th>Notes</th>
+        <th>Force</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr class="bg-warning-custom">
+        <td>4</td>
+        <td>SB001662</td>
+        <td></td>
+        <td>Cannot assign asset to parent: Asset Status was 'Registered', expected: Boxed for Shipping for mode 'Shipping Box Palleting'.</td>
+        <td><button class="btn btn-primary">Force</button></td>
+      </tr>
+      <tr class="bg-success-custom">
+        <td>3</td>
+        <td>SB001662</td>
+        <td></td>
+        <td>SB001662 is already assigned to SP000123</td>
+        <td></td>
+      </tr>
+      </tbody>
+    </table>
+
+    <!-- Inventory List Section -->
+    <h4 v-if="showTransActionsAndInventory">Inventory List</h4>
+    <div class="mb-2" v-if="showTransActionsAndInventory">Inventory list count <input type="text" value="2" class="form-control d-inline w-auto"></div>
+
+    <table class="table table-bordered" v-if="showTransActionsAndInventory">
+      <thead>
+      <tr>
+        <th>Asset ID</th>
+        <th>Status</th>
+        <th>Make</th>
+        <th>Model</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>SB000555</td>
+        <td>Boxed for Shipping</td>
+        <td>null</td>
+        <td>null</td>
+      </tr>
+      <tr>
+        <td>SB001662</td>
+        <td>Boxed for Shipping</td>
+        <td>null</td>
+        <td>null</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
