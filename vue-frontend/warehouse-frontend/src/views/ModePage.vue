5a1dd1dfa9a4b3ebfb9a052c9d1cd553410fa5a1
yohan-oc <129070827+yohan-oc@users.noreply.github.com>
2025-02-07 17:02:09 +0000

    dynamic ui updated

--- vue-frontend/warehouse-frontend/src/views/ModePage.vue
+++ vue-frontend/warehouse-frontend/src/views/ModePage.vue
@@ -4,40 +4,33 @@ export default {
   data() {
     return {
       operationModes: [],
-      groupedModes: {},
     }
   },
   mounted() {
     this.fetchModes();
   },
-  methods: {
-    async fetchModes() {
-      try {
-        const response = await fetch(`/updates/modes.json`);
-        const data = await response.json();
-        //alert(data.operation_modes[0].name);
-        this.operationModes = data.operation_modes;
-        this.groupModesByCategory();
-      } catch (error) {
-        console.error("Error fetching modes:", error);
-      }
-    },
-    groupModesByCategory() {
-      this.groupedModes = this.operationModes.reduce((acc, mode) => {
+  computed: {
+    groupedModes() {
+      return this.operationModes.reduce((acc, mode) => {
         if (!acc[mode.category]) {
           acc[mode.category] = [];
         }
         acc[mode.category].push(mode);
         return acc;
       }, {});
+    }
   },
+  methods: {
+    async fetchModes() {
+      try {
+        const response = await fetch(`/updates/modes.json`);
+        const data = await response.json();
+        this.operationModes = data.operation_modes;
+      } catch (error) {
+        console.error("Error fetching modes:", error);
+      }
+    },
     chooseMode(mode) {
-      // if (this.username) {
-      //   this.$router.push("/mode"); // Navigate to Home after login
-      // } else {
-      //   alert("Please enter username");
-      // }
-      //alert(mode);
       localStorage.setItem('mode', mode);
       this.$router.push("/parent");
     }
@@ -50,93 +43,21 @@ export default {
       <h4 class="section-title">Select a mode</h4>
       <p style="color: #075976;">Begin palleting or by selecting a mode below.</p>
 
-      <div class="row">
-        <div class="col-md-2">
-          <h5 class="section-title">Palleting</h5>
-        </div>
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/work-in-progress.png" alt="Work in Progress" width="50" class="d-block mx-auto">
-            <p>Work in progress palleting</p>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Work in progress palleting')">Start →</button>
+      <div v-for="(modes, category) in groupedModes" :key="category">
+        <div class="row">
+          <div class="col-md-2">
+            <h5 class="section-title">{{ category }}</h5>
           </div>
-        </div>
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/shipping-box.png" alt="Shipping Box" width="50" class="d-block mx-auto">
-            <p>Shipping box palleting</p>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Shipping box palleting')">Start →</button>
+          <div v-for="(mode, index) in modes" :key="mode.name" :class="['col-md-2', { 'offset-md-2': index !== 0 && index % 4 === 0 }]" style="padding-bottom: 25px;">
+            <div class="card">
+              <img :src="require(`@/assets/${mode.imagePath || mode.image}`)" :alt="mode.name" width="50" class="d-block mx-auto">
+              <p>{{ mode.name }}</p>
+              <button class="btn btn-outline-success w-100" @click="chooseMode(mode.name)">Start →</button>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="row mt-4">
-        <div class="col-md-2">
-          <h5 class="section-title">Boxing</h5>
-        </div>
-
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/desktop.png" alt="Wiped Desktops" width="50" class="d-block mx-auto">
-            <p>Boxing (Wiped) Desktops for shipping</p>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Wiped) Desktops for shipping')">Start →</button>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/desktop-imaged.png" alt="Imaged Desktops" width="50" class="d-block mx-auto">
-            <p>Boxing (Imaged) Desktops for shipping</p>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Imaged) Desktops for shipping')">Start →</button>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/laptop.png" alt="Wiped Laptops" width="50" class="d-block mx-auto">
-            <br/>
-            <p>Boxing (Wiped) Laptops for shipping</p>
-            <br/>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Wiped) Laptops for shipping')">Start →</button>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/laptop-imaged.png" alt="Imaged Laptops" width="50" class="d-block mx-auto">
-            <p>Boxing (Imaged) Laptops for shipping</p>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Imaged) Laptops for shipping')">Start →</button>
-          </div>
-        </div>
-      </div>
-
-      <div class="row mt-4 mb-5">
-        <div class="col-md-2"></div>
-
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/monitor.png" alt="Monitors" width="50" class="d-block mx-auto">
-            <p>Boxing monitors for shipping</p>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing monitors for shipping')">Start →</button>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/phone.png" alt="Phones and Laptops" width="50" class="d-block mx-auto">
-            <p>Boxing (Wiped) phones and laptops for shipping</p>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Wiped) phones and laptops for shipping')">Start →</button>
-          </div>
-        </div>
-
-        <div class="col-md-2">
-          <div class="card">
-            <img src="../assets/money.png" alt="For Sale" width="50" class="d-block mx-auto">
-            <p>Boxing Desktops and Laptops for sale</p>
-            <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing Desktops and Laptops for sale')">Start →</button>
-          </div>
-        </div>
-      </div>
     </div>
 </template>
 
@@ -174,3 +95,98 @@ export default {
   border-bottom-color: #04A27D;
 }
 </style>
+
+<!--<template>-->
+<!--  <div class="container">-->
+<!--    <h4 class="section-title">Select a mode</h4>-->
+<!--    <p style="color: #075976;">Begin palleting or by selecting a mode below.</p>-->
+
+<!--    <div class="row">-->
+<!--      <div class="col-md-2">-->
+<!--        <h5 class="section-title">Palleting</h5>-->
+<!--      </div>-->
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/work-in-progress.png" alt="Work in Progress" width="50" class="d-block mx-auto">-->
+<!--          <p>Work in progress palleting</p>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Work in progress palleting')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/shipping-box.png" alt="Shipping Box" width="50" class="d-block mx-auto">-->
+<!--          <p>Shipping box palleting</p>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Shipping box palleting')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+
+<!--    <div class="row mt-4">-->
+<!--      <div class="col-md-2">-->
+<!--        <h5 class="section-title">Boxing</h5>-->
+<!--      </div>-->
+
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/desktop.png" alt="Wiped Desktops" width="50" class="d-block mx-auto">-->
+<!--          <p>Boxing (Wiped) Desktops for shipping</p>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Wiped) Desktops for shipping')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/desktop-imaged.png" alt="Imaged Desktops" width="50" class="d-block mx-auto">-->
+<!--          <p>Boxing (Imaged) Desktops for shipping</p>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Imaged) Desktops for shipping')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/laptop.png" alt="Wiped Laptops" width="50" class="d-block mx-auto">-->
+<!--          <br/>-->
+<!--          <p>Boxing (Wiped) Laptops for shipping</p>-->
+<!--          <br/>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Wiped) Laptops for shipping')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/laptop-imaged.png" alt="Imaged Laptops" width="50" class="d-block mx-auto">-->
+<!--          <p>Boxing (Imaged) Laptops for shipping</p>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Imaged) Laptops for shipping')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+
+<!--    <div class="row mt-4 mb-5">-->
+<!--      <div class="col-md-2"></div>-->
+
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/monitor.png" alt="Monitors" width="50" class="d-block mx-auto">-->
+<!--          <p>Boxing monitors for shipping</p>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing monitors for shipping')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/phone.png" alt="Phones and Laptops" width="50" class="d-block mx-auto">-->
+<!--          <p>Boxing (Wiped) phones and laptops for shipping</p>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing (Wiped) phones and laptops for shipping')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+
+<!--      <div class="col-md-2">-->
+<!--        <div class="card">-->
+<!--          <img src="../assets/money.png" alt="For Sale" width="50" class="d-block mx-auto">-->
+<!--          <p>Boxing Desktops and Laptops for sale</p>-->
+<!--          <button class="btn btn-outline-success w-100" @click="chooseMode('Boxing Desktops and Laptops for sale')">Start →</button>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
